7b796e48bd72d6e76d050e7a628dfb9b55349310
Panda <traitnil@gmail.com>
2022-02-15 13:31:50 +0200

    destructuring

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,22 +9,29 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-email.addEventListener('blur', (event) => {
+function handleBlurEmail(event) {
   const value = event.target.value;
   const isValid = value.includes('@') && value.includes('.');
   if (isValid) {
-    event.target.style.borderColor = 'green';
+    event.target.classList.add('valid')
+    event.target.classList.remove('invalid')
   } else {
-    event.target.style.borderColor = 'red';
+    event.target.classList.add('invalid')
+    event.target.classList.remove('valid')
   }
-});
-password.addEventListener('blur', (event) => {
+}
+function handleBlurPassword(event) {
   const specialChars = ['`', '!', '@', '#', '$', '%', '^', '&', '*', '('];
   const value = event.target.value;
   const isValid = specialChars.some((char) => value.includes(char));
   if (isValid) {
-    event.target.style.borderColor = 'green';
+    event.target.classList.add('valid')
+    event.target.classList.remove('invalid')
   } else {
-    event.target.style.borderColor = 'red';
+    event.target.classList.add('invalid')
+    event.target.classList.remove('valid')
   }
-});
+}
+
+email.addEventListener('blur', handleBlurEmail);
+password.addEventListener('blur', handleBlurPassword);
